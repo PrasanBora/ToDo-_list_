902a14ae542b157ec266067a16ecfc83d31af8fe
PrasanBora <prasanbora23@gmail.com>
2022-08-21 07:48:03 +0530

    Inserting new elements

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 const express =require("express");
 const bodyParser =require("body-parser");
-var items=[];
+const mongoose=require("mongoose");
+const { stringify } = require("querystring");
+
+// var items=[];
 const app =express();
 
 
@@ -9,54 +12,62 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static('public'));
 
+mongoose.connect("mongodb://localhost:27017/to_dolistDB",{useNewUrlParser:true});
+
+const itemsSchema={
+    name:String
+};
+const Item =mongoose.model
+(
+    "item",itemsSchema
+);
+
+const item1=new Item ({name:"Welcome to TODo List"})
+const item2 =new Item ({name:"Press + to add a item"})
+const item3=new Item ({ name:"Hope you like it "});
+
+const defaultItems=[item1,item2,item3];
+
 
 app.get("/", function (req ,res ){
+  
     var today=new Date();
-
-    // var currentDay =today.getDay();
-    // var day="";
-    // switch(currentDay){
-    //     case 0 :
-    //         day="Sunday";
-    //         break;
-    //     case 1 :
-    //         day="Monday";
-    //         break;           
-    //     case 2 :
-    //          day="Tuesday";
-    //         break;
-    //     case 3 :
-    //         day="Wednesday";
-    //         break;
-    //     case 4 :
-    //         day="Thursday";
-    //         break;
-    //     case 5 :
-    //         day="Friday";
-    //         break;     
-    //      case 6:
-    //         day="Saturday";
-    //         break;  
-         
-    //     default:
-    //         console.log("Error , invalid switch case ! ");}
-
     const options = { weekday: 'long',  month: 'long', day: 'numeric' };
 
-    var day = today.toLocaleDateString("en-US",options);
+    Item.find({},function(err,foundItems){
+        console.log(foundItems);
 
+        var day = today.toLocaleDateString("en-US",options);
     
-    res.render("list",{tempday:day , newListItems:items})
+        if(foundItems.length==0)
+        {
+            Item.insertMany(defaultItems, function(err){
+                if(err)
+                { console.log(err);}
+                else 
+                console.log("Sucess in defaults item");
+            });
+            res.redirect("/");
+        }
+        else 
+        res.render("list",{tempday:day , newListItems:foundItems})
+    })
+
+   
 });
   
 // to handel post request 
 
 app.post("/",function(req,res){
     // console.log(req.body.newItem);
-     var item=req.body.newItem;
+     const itemName=req.body.newItem;
 
-    items.push(item);  
-    res.redirect("/");
+     const item= new Item ({
+        name:itemName
+     }); 
+     item.save();
+     res.redirect("/");
+     
 })
  
 
